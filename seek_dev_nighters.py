d02fc72f606ff8d220af518dcfbd17554de394bb
Ira <you@example.com>
2017-10-19 01:57:52 +0300

    dirty version

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,25 @@
+import requests
+import pytz
+from datetime import datetime, timezone
+
 def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+    pages = 10
+    for page in range(1, pages):
+        r = requests.get('https://devman.org/api/challenges/solution_attempts/?page={}'.format(page))
+        for user in r.json()['records']:
+            yield user
 
 def get_midnighters():
-    pass
+    midnighters = []
+    today = datetime.now(timezone.utc)
+    for i in load_attempts():
+        user_timezone = pytz.timezone(i['timezone'])
+        published_date = user_timezone.localize(datetime.fromtimestamp(i['timestamp']))
+        if (today - published_date).days > 3:
+            break
+        if published_date.hour >=0 and published_date.hour <=7:
+            midnighters.append((i['username'], published_date))
+    return midnighters
 
 if __name__ == '__main__':
-  pass
+    print(get_midnighters())
