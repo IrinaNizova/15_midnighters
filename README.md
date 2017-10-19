45379b30ad578f38bc59ad073236184106d3b560
Ira <you@example.com>
2017-10-19 15:47:40 +0300

    create task

--- README.md
+++ README.md
@@ -1,6 +1,34 @@
 # Night Owls Detector
 
-[TODO. There will be project description]
+This script print information about users, who post their task on the site devman.org last week at night (0 - 7 am).
+
+# How to start
+
+```python
+python seek_dev_nighters.py
+```
+
+# Examples of output
+```
+There 5 unique users for the last week, who send tasks at night
+АртемЧерняев
+SergeiKhrustalev
+АлексейЦыкунов
+sokolovav2016
+kazimir_de_solar
+These tasks are:
+User SergeiKhrustalev send task on 00:57 19.10.2017
+User kazimir_de_solar send task on 00:41 19.10.2017
+User sokolovav2016 send task on 00:06 19.10.2017
+User АлексейЦыкунов send task on 01:21 18.10.2017
+User SergeiKhrustalev send task on 01:15 18.10.2017
+User SergeiKhrustalev send task on 01:02 18.10.2017
+User SergeiKhrustalev send task on 00:35 18.10.2017
+User АлексейЦыкунов send task on 00:18 18.10.2017
+User SergeiKhrustalev send task on 00:09 18.10.2017
+User АртемЧерняев send task on 00:28 17.10.2017
+```
+
 
 # Project Goals
 
--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -2,12 +2,15 @@ import requests
 import pytz
 from datetime import datetime, timezone
 
+
 def load_attempts():
     pages = 10
     for page in range(1, pages):
-        r = requests.get('https://devman.org/api/challenges/solution_attempts/?page={}'.format(page))
-        for user in r.json()['records']:
+        url = 'https://devman.org/api/challenges/solution_attempts/?page={}'
+        users = requests.get(url.format(page))
+        for user in users.json()['records']:
             yield user
+
 
 def get_midnighters():
     midnighters = []
@@ -15,11 +18,23 @@ def get_midnighters():
     for i in load_attempts():
         user_timezone = pytz.timezone(i['timezone'])
         published_date = user_timezone.localize(datetime.fromtimestamp(i['timestamp']))
-        if (today - published_date).days > 3:
+        # I limited the information about users to one week
+        if (today - published_date).days > 7:
             break
-        if published_date.hour >=0 and published_date.hour <=7:
-            midnighters.append((i['username'], published_date))
+        if published_date.hour >= 0 and published_date.hour < 7:
+            midnighters.append({'username': i['username'], 'date': published_date})
     return midnighters
 
+
 if __name__ == '__main__':
-    print(get_midnighters())
+    midnighters = get_midnighters()
+    uniq_users = set([midnighter['username'] for midnighter in midnighters])
+    print('There {} unique users for the last week, who send tasks at night'
+          .format(len(uniq_users)))
+    for user in uniq_users:
+        print(user)
+    print('These tasks are:')
+    for midnighter in midnighters:
+        print('User {} send task on {}'
+              .format(midnighter['username'],
+                      midnighter['date'].strftime("%H:%M %d.%m.%Y")))
