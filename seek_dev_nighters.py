da78515b6fd59f1ce044f754dd56b8d9376eaa35
Ira <you@example.com>
2017-10-21 23:07:47 +0300

    fix bugs

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -3,7 +3,7 @@ import pytz
 from datetime import datetime, timezone
 
 
-def get_json_from_devman(page):
+def get_user_list_from_devman(page):
     url = 'https://devman.org/api/challenges/solution_attempts/'
     return requests.get(url, {'page': page}).json()
 
@@ -21,28 +21,31 @@ def get_user_data(json):
         yield user
 
 
-def get_midnighters():
+def get_midnighter_commits():
     midnighters = []
-    first_page = 1
-    pages = get_json_from_devman(first_page)["number_of_pages"]
-    for page in range(1, pages+1):
-        json = get_json_from_devman(page)
-        for user in get_user_data(json):
+    first_page_num = 1
+    first_page = get_user_list_from_devman(first_page_num)
+    count_pages = first_page["number_of_pages"]
+    for page in range(1, count_pages+1):
+        user_list = first_page if page==1 else get_user_list_from_devman(page)
+        for user in get_user_data(user_list):
             if user['is_night']:
                 midnighters.append({'username': user['username'],
-                                    'date': user['published_date']})
+                                    'published_date': user['published_date']})
     return midnighters
 
 
 if __name__ == '__main__':
-    midnighters = get_midnighters()
-    uniq_users = set([midnighter['username'] for midnighter in midnighters])
-    print('There {} unique users, who send tasks at night'
+    midnighter_commits = get_midnighter_commits()
+    uniq_users = set([commit['username'] for commit in midnighter_commits])
+    print('There {} unique users, who create commits at night'
           .format(len(uniq_users)))
-    for user in uniq_users:
-        print(user)
-    print('These tasks are:')
-    for midnighter in midnighters:
+    for uniq_user in uniq_users:
+        print(uniq_user)
+
+    print('List of commits:')
+    for midnighter_commit in midnighter_commits:
         print('User {} send task on {}'
-              .format(midnighter['username'],
-                      midnighter['date'].strftime("%H:%M %d.%m.%Y")))
+              .format(midnighter_commit['username'],
+                      midnighter_commit['published_date']
+                      .strftime("%H:%M %d.%m.%Y")))
