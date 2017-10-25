5c0f923982dbeedae36c45a28957d4c7ee143a2a
Ira <you@example.com>
2017-10-25 20:09:18 +0300

    fix bugs

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -8,31 +8,28 @@ def get_user_list_from_devman(page):
     return requests.get(url, {'page': page}).json()
 
 
-def get_user_data(json):
-    for user in json['records']:
+def get_user_data(user_records):
+    for user in user_records:
         user_timezone = pytz.timezone(user['timezone'])
-        published_date = user_timezone.localize(
+        pub_date = user_timezone.localize(
             datetime.fromtimestamp(user['timestamp']))
-        user['published_date'] = published_date
-        if published_date .hour >= 0 and published_date.hour < 7:
-            user['is_night'] = True
-        else:
-            user['is_night'] = False
+        user['published_date'] = pub_date
+        user['is_night'] = True if pub_date.hour >= 0 and pub_date.hour < 7 else False
         yield user
 
 
 def get_midnighter_commits():
-    midnighters = []
+    midnighter_commits = []
     first_page_num = 1
     first_page = get_user_list_from_devman(first_page_num)
     count_pages = first_page["number_of_pages"]
     for page in range(1, count_pages+1):
         user_list = first_page if page==1 else get_user_list_from_devman(page)
-        for user in get_user_data(user_list):
+        for user in get_user_data(user_list['records']):
             if user['is_night']:
-                midnighters.append({'username': user['username'],
+                midnighter_commits.append({'username': user['username'],
                                     'published_date': user['published_date']})
-    return midnighters
+    return midnighter_commits
 
 
 if __name__ == '__main__':
